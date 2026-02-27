77feb174ed2b27181d9a34e3e418203d2aabe631
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-02-27 23:34:37 +0600

    adding creating data in data

--- CREATE AND INSERT DATA  IN DATABASE.sql
+++ CREATE AND INSERT DATA  IN DATABASE.sql
@@ -5,9 +5,26 @@ name 	VARCHAR(50),
 age INT NOT NULL 
 );
 
-INSERT INTO student VALUE (1,"RASHEDUL ISLAM",26);
-INSERT INTO student VALUE (2 , "ROMMAN",25);
+-- INSERT INTO student VALUE (1,"RASHEDUL ISLAM",26);
+-- INSERT INTO student VALUE (2 , "ROMMAN",25);
+
+-- SELECT *
+-- FROM student;
+
+-- SHOW DATABASES; 
+-- DROP TABLE nothing;
+
+-- CREATE TABLE student (
+-- rollbo INT PRIMARY  KEY ,
+-- name VARCHAR(50)
+-- );
 
 
-SELECT *
-FROM student;
+-- ! INSERT TABLE DATA 
+SELECT * FROM student;
+
+-- insert data in table;
+ INSERT INTO student(rollname,name)VALUE (101,"Rashedul"),(102,"Abdullah"),(103,"Romman");
+
+
+ 
