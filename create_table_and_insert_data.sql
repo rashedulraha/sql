15cc2481b0bea4d36efa3746ce45a550d74b6c13
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-02-28 16:04:57 +0600

    insert data

---
+++ create_table_and_insert_data.sql
@@ -0,0 +1,29 @@
+-- create database
+CREATE DATABASE collage ;
+ -- use database 
+USE collage ;
+
+-- CREATE TABLE student 
+CREATE TABLE student (
+rollno INT PRIMARY KEY,
+name VARCHAR(50),
+marks INT NOT NULL , 
+grade VARCHAR(1),city VARCHAR(20)
+);
+-- DROP TABLE student;
+
+-- SELECT AND GET ALL TABLE DATA 
+SELECT * FROM student ;
+
+-- INSERT INTO TABLE DATA 
+INSERT INTO student (rollno  , name, marks , grade , city ) VALUE
+ (101,"RAHSEDUL",78,"C","NAOGARON"),
+ (102,"Bhumika" , 89,"A","Dhaka"),
+ (103,"Chetan" ,56,"C","Patnitala"),
+ (104,"dharub" ,50,"D","Delhi"),
+ (105,"emanuz" ,90,"A","Gaganpur");
+ 
+
+-- Get all  table data 
+SELECT * FROM student ; 
+
---
+++ primary key and foren key.sql
@@ -0,0 +1,21 @@
+-- CREATE TABLE temp1(
+-- id INT UNIQUE
+-- );
+
+
+-- INSERT INTO temp1 VALUE (101);
+-- INSERT INTO temp1 VALUE (101);
+
+
+--  SELECT * FROM temp1;
+--  
+
+DROP TABLE temp1;
+
+CREATE TABLE temp1(
+id INT ,
+name VARCHAR(50),
+age INT ,
+city VARCHAR(50),
+PRIMARY KEY (id)
+);
