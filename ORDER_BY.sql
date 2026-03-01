727755b47175b5e911097e52189062908b85418b
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-03-01 15:07:19 +0600

    update order by

--- ORDER_BY.sql
+++ ORDER_BY.sql
@@ -1,1 +1,16 @@
-SELECT * FROM student ORDER BY marks DESC LIMIT 4;
+-- SELECT * FROM student ORDER BY marks DESC LIMIT 4;
+USE collage;
+
+
+SELECT * FROM student;
+INSERT INTO student VALUE (202, "Nakib",56,"D","Kholna");
+INSERT INTO student VALUE (205, "",56,"D","Kholna");
+
+--  INSERT DATA ONLY SPECIFIC  COLOUMNS
+
+INSERT INTO student (rollno ,marks ,  grade , city) VALUE (560,89,"D","MANIKGONGO");
+-- insert into multiple vlaue in database 
+INSERT INTO student (rollno , name , marks, grade ,city) VALUE 
+(580,"nothing",89,"A","Dhaka"),
+(581,"nothing more",79,"A","Naogaon"),
+(582,"hello my sql",89,"A","khulna");
